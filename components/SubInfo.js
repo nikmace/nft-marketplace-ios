e0c3dcde845010321a4e801294b6d171f8492892
Nikita Baranov <nikmacentosh@gmail.com>
2022-04-05 16:36:37 +0300

    Custom components used by screens

---
+++ components/SubInfo.js
@@ -0,0 +1,130 @@
+import { View, Text, Image } from "react-native";
+
+import { SIZES, FONTS, COLORS, SHADOWS, assets } from "../constants";
+
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
+  return (
+    <View>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: titleSize,
+          color: COLORS.primary,
+        }}
+      >
+        {title}
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: subTitleSize,
+          color: COLORS.primary,
+        }}
+      >
+        {subTitle}
+      </Text>
+    </View>
+  );
+};
+
+export const EthPrice = ({ price }) => {
+  return (
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image
+        source={assets.eth}
+        resizeMode="contain"
+        style={{ width: 20, height: 20, marginRight: 2 }}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.medium,
+          fontSize: SIZES.font,
+          color: COLORS.primary,
+        }}
+      >
+        {price}
+      </Text>
+    </View>
+  );
+};
+
+export const ImageCmp = ({ imgUrl, idx }) => {
+  return (
+    <Image
+      source={imgUrl}
+      resizeMode="contain"
+      style={{
+        width: 48,
+        height: 48,
+        marginLeft: idx === 0 ? 0 : -SIZES.font,
+      }}
+    />
+  );
+};
+
+export const People = () => {
+  return (
+    <View style={{ flexDirection: "row" }}>
+      {[assets.person01, assets.person02, assets.person03].map(
+        (imgUrl, idx) => (
+          <ImageCmp imgUrl={imgUrl} idx={idx} key={`People-${idx}`} />
+        )
+      )}
+    </View>
+  );
+};
+
+export const EndDate = () => {
+  return (
+    <View
+      style={{
+        paddingHorizontal: SIZES.font,
+        paddingVertical: SIZES.base,
+        backgroundColor: COLORS.white,
+        justifyContent: "center",
+        alignItems: "center",
+        ...SHADOWS.light,
+        elevation: 1,
+        maxWidth: "50%",
+        //TODO: Style this better
+        borderRadius: SIZES.base,
+      }}
+    >
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small,
+          color: COLORS.primary,
+        }}
+      >
+        Ending in
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.medium,
+          color: COLORS.primary,
+        }}
+      >
+        12h 30m
+      </Text>
+    </View>
+  );
+};
+
+export const SubInfo = () => {
+  return (
+    <View
+      style={{
+        width: "100%",
+        paddingHorizontal: SIZES.font,
+        marginTop: -SIZES.extraLarge,
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      <People />
+      <EndDate />
+    </View>
+  );
+};
