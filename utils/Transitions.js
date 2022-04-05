b72d19c1770b3a160b81291b8d073ae2548e696d
Nikita Baranov <nikmacentosh@gmail.com>
2022-04-05 16:44:04 +0300

    Transitions used when navigating to different screens

---
+++ utils/Transitions.js
@@ -0,0 +1,58 @@
+export const TransitionSpec = {
+  animation: "spring",
+  config: {
+    stiffness: 1000,
+    damping: 500,
+    mass: 3,
+    overshootClamping: true,
+    restDisplacementThreshold: 0.01,
+    restSpeedThreshold: 0.01,
+  },
+};
+
+export const TransitionScreen = {
+  gestureDirection: "horizontal",
+  transitionSpec: {
+    open: TransitionSpec,
+    close: TransitionSpec,
+  },
+  cardStyleInterpolator: ({ current, next, layouts }) => {
+    return {
+      cardStyle: {
+        transform: [
+          {
+            translateX: current.progress.interpolate({
+              inputRange: [0, 1],
+              outputRange: [layouts.screen.width, 0],
+            }),
+          },
+          {
+            translateX: next
+              ? next.progress.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: [0, -layouts.screen.width],
+                })
+              : 1,
+          },
+        ],
+      },
+      overlayStyle: {
+        opacity: current.progress.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, 0.5],
+        }),
+      },
+    };
+  },
+};
+
+export const CardOptions = {
+  cardStyle: { backgroundColor: "transparent" },
+  ...TransitionScreen,
+};
+
+export default {
+  TransitionSpec,
+  TransitionScreen,
+  CardOptions,
+};
